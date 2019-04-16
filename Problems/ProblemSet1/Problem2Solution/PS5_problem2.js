3977c847ec9b61ed82382d36f74752ae9e3ae1fd
Kasi Rajan <kasi.rajan@vajro.com>
2019-04-16 14:26:52 +0530

    1. function to generate random number has been done 2. function to read the messages from the other file is done 3. function to print final output is done

--- Problems/ProblemSet1/Problem2Solution/PS5_problem2.js
+++ Problems/ProblemSet1/Problem2Solution/PS5_problem2.js
@@ -4,3 +4,52 @@
 
 
 var readline = require('readline-sync'); // this is to read the inputs from the console
+
+var table_messages_string = "";
+
+// this function generates the random number to pick the messages from the table
+function getRandomNumber(start, end) {
+    return Math.floor(Math.random() * end) + start
+}
+
+function readTableMessages(callback) {
+    var fs = require('fs'),
+        path = require('path'),
+        filePath = path.join(__dirname, 'table_message.txt');
+
+    fs.readFile(filePath, { encoding: 'utf-8' }, function(err, data) {
+        if (err) {
+            console.log("Error in reading table messages data");
+            console.log(err);
+            callback(" ")
+            return;
+        } else {
+            console.log("Data has fetched properly from the file");
+            callback(data);
+            return;
+        }
+    });
+}
+
+function finalOutput(ruler, alices_of_ruler) {
+    console.log("Who is the ruler of Southeros?");
+    if (ruler !== "") {
+        console.log("None");
+    } else {
+        console.log(ruler);
+    }
+    console.log("Allies of Ruler?");
+    if (ruler !== "" || alices_of_ruler.length <= 0) {
+        console.log("None");
+    } else {
+        console.log(alices_of_ruler.toString());
+    }
+}
+
+function testingFunction() {
+    readTableMessages(function(data) {
+        console.log(data);
+    });
+}
+
+testingFunction();
